11b61c1f592a003760db48133559d0c027ec9cc4
Kunnadi <theliteraryagrarian@gmail.com>
2019-04-05 16:37:23 -0500

    Importing functions from diffrent scripts,set up the way I think it was meant to

--- translate_orf.py
+++ translate_orf.py
@@ -1,1 +1,80 @@
-new stuff
+#! /usr/bin/env python3
+
+import find_orf.py
+find_first_orf()
+import translate.py
+translate_sequence()
+import find_orf.py
+parse_sequence_from_path()
+
+
+def main():
+    import sys
+    import argparse
+
+    # Create a command-line parser object
+    parser = argparse.ArgumentParser(
+            formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+
+    # Tell the parser what command-line arguments this script can receive
+    parser.add_argument('sequence',
+            metavar = 'SEQUENCE',
+            type = str,
+            help = ('The sequence to search for an open-reading frame. '
+                    'If the path flag (\'-p\'/\'--path\') is specified, '
+                    'then this should be a path to a file containing the '
+                    'sequence to be searched.'))
+    parser.add_argument('-p', '--path',
+            action = 'store_true',
+            help = ('The sequence argument should be treated as a path to a '
+                    'containing the sequence to be searched.'))
+    parser.add_argument('-s', '--start-codons',
+            type = str,
+            nargs = '+', # one or more arguments
+            default = ['AUG'],
+            help = ('One or more possible start codons.'))
+    parser.add_argument('-x', '--stop-codons',
+            type = str,
+            nargs = '+', # one or more arguments
+            default = ['UAA', 'UAG', 'UGA'],
+            help = ('One or more possible stop codons.'))
+
+    # Parse the command-line arguments into a 'dict'-like container
+    args = parser.parse_args()
+
+    # Check to see if the path option was set to True by the caller. If so, parse
+    # the sequence from the path
+    if args.path:
+        sequence = parse_sequence_from_path(args.sequence)
+    else:
+    sequence = args.sequence
+
+    orf = find_first_orf(sequence = sequence,
+            start_codons = args.start_codons,
+            stop_codons = args.stop_codons)
+    sys.stdout.write('{}\n'.format(orf))
+
+
+if __name__ == '__main__':
+    main()
+   genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'AC$
+    rna_seq = ("AUG"
+            "UAC"
+            "UGG"
+            "CAC"
+            "GCU"
+            "ACU"
+            "GCU"
+            "CCA"
+            "UAU"
+            "ACU"
+            "CAC"
+            "CAG"
+            "AAU"
+            "AUC"
+            "AGU"
+            "ACA"
+            "GCG")
+    longest_peptide = get_longest_peptide(rna_sequence = rna_seq,
+            genetic_code = genetic_code)
+    
